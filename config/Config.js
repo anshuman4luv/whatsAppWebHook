47e77dea62e7865fd5be19afffe1a6fdf9d40bcd
anshuman4luv <anshuman4luv@gmail.com>
2019-06-21 15:44:37 +0530

    Update Config.js

--- config/Config.js
+++ config/Config.js
@@ -2,6 +2,7 @@
 module.exports.ODA_WEBHOOK_URL = process.env.BOT_WEBHOOK_URL || 'https://botv2frk1I0208H4446A1bots-mpaasocimt.botmxp.ocp.oraclecloud.com:443/connectors/v1/tenants/idcs-6d466372210e4300bb31f4db15e8e96c/listeners/webhook/channels/e215d955-6faa-43bf-b2ab-016db870dd1b';
 module.exports.ODA_WEBHOOK_SECRET = process.env.BOT_WEBHOOK_SECRET || 'mRLawCclQIckkIabNNrx0rbyfU1etfYx';
 
+
 // Smooch Details
 exports.SMOOCH_APP_ID = process.env.SMOOCH_APP_ID || '5d0bad567d1721001080a32c';
 exports.SMOOCH_KEY_ID = process.env.SMOOCH_KEY_ID || 'app_5d0c8f4fdda1fb001059f7e3';
